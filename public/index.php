b463f3e80b9c6afb30196acc3e0024740904f5fd
Edwin Bolt <edwin@optiwise.nl>
2020-08-03 11:06:58 +0200

    'quickcommit'

--- public/index.php
+++ public/index.php
@@ -19,9 +19,10 @@ if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
 if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
     Request::setTrustedHosts([$trustedHosts]);
 }
-
 $kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
+
